2cb3867eb5c3158dbcafe987dba80e2a87abb290
Alejandro Gómez <alejandroogomez@gmail.com>
2012-01-23 00:19:00 +0100

    constants

---
+++ turses/constant.py
@@ -0,0 +1,24 @@
+###############################################################################
+#                               coding=utf-8                                  #
+#           Copyright (c) 2012 Nicolas Paris and Alejandro Gómez.             #
+#       Licensed under the GPL License. See LICENSE.txt for full details.     #
+###############################################################################
+
+palette = [
+    ('body'          , 'default'   , '', 'standout') ,
+    ('focus'         , 'dark red'  , '', 'standout') ,
+    ('header'        , 'light blue', ''),
+    ('line'          , 'dark blue' , ''),
+    ('info_msg'      , 'dark green', ''),
+    ('warn_msg'      , 'dark red'  , ''),
+    ('active_tab'    , 'light blue', ''),
+    ('inactive_tab'  , 'dark blue' , ''),
+    ('read'          , 'dark blue' , ''),
+    ('unread'        , 'dark red'  , ''),
+    ('hashtag'       , 'dark green', ''),
+    ('attag'         , 'brown'     , ''),
+    ('highlight'     , 'dark red'  , ''),
+    ('highlight_nick', 'light red' , ''),
+    ('header'        , 'white'     , 'black'),
+    ('reveal focus'  , 'black'     , 'dark cyan', 'standout') ,
+]
