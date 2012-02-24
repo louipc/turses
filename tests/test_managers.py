9716e9d0ccf02f040cdb30b91728bb5464707ecc
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-24 17:51:41 +0100

    test for `TimelineManager`

--- tests/test_managers.py
+++ tests/test_managers.py
@@ -8,16 +8,34 @@ import sys
 sys.path.append('../')
 import unittest
 
+from mock import MagicMock
+
 from turses.timeline import Timeline
 from turses.managers import TimelineManager
 
 class TimelineManagerTest(unittest.TestCase):
     def setUp(self):
         self.timeline = Timeline()
-        self.TimelineManager(self.timeline, lambda x:[])
 
-    # TODO
-    #  mocks or stubs
+    def test_update_with_no_args(self):
+        mock = MagicMock(name='update')
+        self.timeline_manager = TimelineManager(self.timeline, mock)
+        self.timeline_manager.update()
+        mock.assert_called_once_with()
+
+    def test_update_with_one_arg(self):
+        mock = MagicMock(name='update')
+        args = '#python'
+        self.timeline_manager = TimelineManager(self.timeline, mock, args)
+        self.timeline_manager.update()
+        mock.assert_called_once_with(args)
+
+    def test_update_with_multiple_args(self):
+        mock = MagicMock(name='update')
+        args = ('#python', '#mock')
+        self.timeline_manager = TimelineManager(self.timeline, mock, args)
+        self.timeline_manager.update()
+        mock.assert_called_once_with(args)
         
 
 if __name__ == '__main__':
