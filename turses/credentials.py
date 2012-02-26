752a698c97be812c71110695391e4aa967936282
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-26 02:04:33 +0100

    `tuitclient` credentials

--- turses/credentials.py
+++ turses/credentials.py
@@ -6,6 +6,5 @@
 
 consumer_key='OEn4hrNGknVz9ozQytoR0A'
 consumer_secret='viud49uVgdVO9dnOGxSQJRo7jphTioIlEn3OdpkZI'
-access_token_key='<insert-yours-here>'
-access_token_secret='<insert-yours-here>'
-
+access_token_key='468287156-q32uqQguQxSEJCjgvSjWM5fSq9LUXYbxuO6bQQB4'
+access_token_secret='yN7v1ImirPPUM8WFLntm9A7xBLTKfBaoCa7NQfsJWo0'
