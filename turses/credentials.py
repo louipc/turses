1c7c5a965652c5d30ddf7a89c982bf859af6a75d
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-26 02:02:11 +0100

    credentials

---
+++ turses/credentials.py
@@ -0,0 +1,11 @@
+###############################################################################
+#                               coding=utf-8                                  #
+#                     Copyright (c) 2012 Alejandro Gómez.                     #
+#       Licensed under the GPL License. See LICENSE.txt for full details.     #
+###############################################################################
+
+consumer_key='OEn4hrNGknVz9ozQytoR0A'
+consumer_secret='viud49uVgdVO9dnOGxSQJRo7jphTioIlEn3OdpkZI'
+access_token_key='<insert-yours-here>'
+access_token_secret='<insert-yours-here>'
+
