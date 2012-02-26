0bdb267e08f75120a89ef4c95e091a36bf3129a1
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-26 20:58:14 +0100

    packaging bare minimum

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='turses',
+    version='alpha',
+    packages=['turses',],
+    license='GNU General Public License v2',
+    long_description=open('README.md').read(),
+)
