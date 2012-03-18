98c5158c0b3d5ee607ff11c34a61f65ca8a88e0a
Alejandro Gómez <alejandroogomez@gmail.com>
2012-03-18 13:19:36 +0100

    add some abstract widgets

--- turses/ui/base.py
+++ turses/ui/base.py
@@ -97,3 +97,63 @@ class UserInterface(object):
 
     def disconnect_editor_done_signal(self, done_signal_handler):
         raise NotImplementedError
+
+
+class Tabs(object):
+    """
+    A widget that renders tabs with the given strings as titles.
+
+    One of them is highlighted as the active tab.
+    """
+    def append_tab(self, tab):
+        raise NotImplementedError
+
+    def delete_current_tab(self):
+        raise NotImplementedError
+
+    def set_active_tab(self, pos):
+        raise NotImplementedError
+
+    def set_tabs(self, tabs):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+
+class Status(object):
+    """Displays a text with context-aware attributes."""
+
+    def message(self, text):
+        """Show `text` as the message.""" 
+        raise NotImplementedError
+
+    def error_message(self, text):
+        raise NotImplementedError
+
+    def info_message(self, text):
+        raise NotImplementedError
+
+    def clear(self):
+        """Clear the text."""
+        raise NotImplementedError
+
+
+class Scrollable(object):
+    """A set of methods that the scrollable widgets must implement."""
+
+    def scroll_up(self):
+        """Sets the focus in the previous element (if any) of the widget."""
+        raise NotImplementedError
+
+    def scroll_down(self):
+        """Sets the focus in the next element (if any) of the widget."""
+        raise NotImplementedError
+
+    def scroll_top(self):
+        """Sets the focus in the first element (if any) of the widget."""
+        raise NotImplementedError
+
+    def scroll_bottom(self):
+        """Sets the focus in the last element (if any) of the widget."""
+        raise NotImplementedError
