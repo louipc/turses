40962544e672a5bbfda9c8133d0d4983c8e14572
Alejandro Gómez <alejandroogomez@gmail.com>
2012-05-11 12:12:33 +0200

    set console title

--- turses/cli.py
+++ turses/cli.py
@@ -7,8 +7,11 @@ turses.cli
 Handle the invocation of `turses` from the command line.
 """
 
+from sys import stdout
+
 from urwid import set_encoding
 
+from turses import __name__
 from turses.utils import parse_arguments
 from turses.config import Configuration
 from turses.ui import CursesInterface
@@ -16,8 +19,15 @@ from turses.api.backends import TweepyApi
 from turses.core import Turses
 
 
+def set_title(string):
+    try:
+        stdout.write("\x1b]2;%s\x07" % string)
+    except:
+        pass
+
 def main():
     try:
+        set_title(__name__)
         set_encoding('utf8')
 
         args = parse_arguments()
