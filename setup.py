048bd1058c1aef42ed9b52d010b6dc5511af7125
Alejandro Gómez <alejandroogomez@gmail.com>
2012-07-24 00:40:05 +0200

    Improve `setup.py`
    I've included some information about `turses` in the docstring of `setup.py` so
    it can be the long description in case that opening the `README` file fails.
    
    Besides that I added `tox` as a requirement for testing, added a keyword and
    modified the short description.

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """
-turses installation.
+turses
+======
+
+A Twitter client for the console.
+
+The goal of the project is to build a full-featured, lightweight, and extremely 
+configurable Twitter client.
+
+Documentation
+-------------
+
+The documentation for ``turses`` is `available on ReadTheDocs
+<http://turses.readthedocs.org>`_.
+
+License
+-------
+
+``turses`` is licensed under a GPLv3 license, see ``LICENSE`` for details.
+
+Authors
+-------
+
+``turses`` is based on `Tyrs`_ by `Nicolas Paris`_.
+
+.. _`Tyrs`: http://tyrs.nicosphere.net
+.. _`Nicolas Paris`: http://github.com/Nic0
+
+See ``AUTHORS`` for a full list of contributors.
 """
 
 from setuptools import setup, find_packages
@@ -16,12 +43,12 @@ REQUIREMENTS = [
     "tweepy",
 ]
 TEST_REQUIREMENTS = list(REQUIREMENTS)
-TEST_REQUIREMENTS.extend(["mock", "nose", "coverage"])
+TEST_REQUIREMENTS.extend(["mock", "nose", "coverage", "tox"])
 
 try:
     long_description = open("README.rst").read() + "\n\n" + open("HISTORY.rst").read()
 except IOError:
-    long_description = ""
+    long_description = __doc__
 
 
 setup(name=NAME,
@@ -29,9 +56,9 @@ setup(name=NAME,
       author="Alejandro Gómez",
       author_email="alejandroogomez@gmail.com",
       url="http://github.com/alejandrogomez/turses",
-      description="A Twitter client with a curses interface.",
+      description="A Twitter client for the console.",
       long_description=long_description,
-      keywords="twitter client, curses, console",
+      keywords="twitter client, curses, console, twitter",
       packages=find_packages(),
       entry_points={
           'console_scripts':
