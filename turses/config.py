louipc/turses

commit bfc1d3160ef7883c2cd0efc81d89a33d4cc2ba7b

python3 port

turses/config.py
@@ -57,9 +57,14 @@ If you want to generate a configuration file, you can do so executing:
 
     $ turses -g /path/to/file
 """
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+from builtins import object
 
 from sys import exit
-from ConfigParser import RawConfigParser
+from configparser import RawConfigParser
 from os import getenv, path, mkdir, remove
 from functools import partial
 from gettext import gettext as _
@@ -398,13 +403,13 @@ SECTION_TOKEN = 'token'
 
 
 def print_deprecation_notice():
-    print "NOTE:"
-    print
-    print "The configuration file in %s has been deprecated." % (
-        LEGACY_CONFIG_FILE)
-    print "A new configuration directory is being generated in %s." % (
-        CONFIG_PATH)
-    print
+    print("NOTE:")
+    print()
+    print("The configuration file in %s has been deprecated." % (
+        LEGACY_CONFIG_FILE))
+    print("A new configuration directory is being generated in %s." % (
+        CONFIG_PATH))
+    print()
 
 
 def invert_command_map(bindings):
@@ -412,7 +417,7 @@ def invert_command_map(bindings):
     Invert configuration keybindings to make reverse lookups faster
     """
     command_map = {}
-    for command, (key, _) in bindings.iteritems():
+    for command, (key, _) in bindings.items():
         command_map[key] = command
     return command_map
 
@@ -460,8 +465,8 @@ class Configuration(object):
             try:
                 mkdir(CONFIG_PATH)
             except:
-                print encode(_('Error creating config directory in %s' % (
-                    CONFIG_DIR)))
+                print(encode(_('Error creating config directory in %s' % (
+                    CONFIG_DIR))))
                 self.exit_with_code(3)
 
     def parse_args(self, cli_args):
@@ -673,15 +678,15 @@ class Configuration(object):
         self._add_section_styles(conf)
         self._add_section_debug(conf)
 
-        with open(config_file, 'wb') as config:
+        with open(config_file, 'w') as config:
             conf.write(config)
 
     def _config_generation_success(self, config_file):
-        print encode(_('Generated configuration file in %s')) % config_file
+        print(encode(_('Generated configuration file in %s')) % config_file)
 
     def _config_generation_error(self, config_file):
-        print encode(_('Unable to generate configuration file in %s')) % (
-            config_file)
+        print(encode(_('Unable to generate configuration file in %s')) % (
+            config_file))
         self.exit_with_code(2)
 
     def generate_token_file(self,
@@ -696,10 +701,10 @@ class Configuration(object):
         conf.set(SECTION_TOKEN, 'oauth_token', oauth_token)
         conf.set(SECTION_TOKEN, 'oauth_token_secret', oauth_token_secret)
 
-        with open(token_file, 'wb') as tokens:
+        with open(token_file, 'w') as tokens:
             conf.write(tokens)
 
-        print encode(_('your account has been saved'))
+        print(encode(_('your account has been saved')))
 
     def parse_config_file(self, config_file):
         conf = RawConfigParser()
@@ -751,7 +756,7 @@ class Configuration(object):
                 elif style == 'statuses_in_user_info':
                     self.styles[style] = conf.getint(SECTION_STYLES, style)
                 else:
-                    self.styles[style] = unicode(
+                    self.styles[style] = str(
                         conf.get(SECTION_STYLES, style), 'utf-8')
 
     def _parse_debug(self, conf):

Makefile
@@ -1,11 +1,11 @@
 APPNAME=turses
 
 PY=python
-DIST=$(PY) setup.py sdist
+DIST=python setup.py sdist
 PIPI=pip install
 PIPFLAGS=--ignore-installed --no-deps
 
-TESTRUNNER=py.test
+TESTRUNNER=pytest
 COVERTESTFLAGS=--with-coverage --cover-package=turses --cover-html
 WATCHTESTFLAGS=--verbosity=0
 

tests/__init__.py
@@ -1,34 +1,1 @@
-from datetime import datetime
-from time import sleep
-
-from turses.models import Status, DirectMessage
-
-
-def now():
-    sleep(0.001)
-    return datetime.now()
-
-
-def create_status(**kwargs):
-    defaults = {
-        'id': 1,
-        'created_at': now(),
-        'user': 'testbot',
-        'text': 'Status created at %s' % now,
-    }
-    defaults.update(**kwargs)
-
-    return Status(**defaults)
-
-
-def create_direct_message(**kwargs):
-    defaults = {
-        'id': 1,
-        'created_at': now(),
-        'sender_screen_name': 'Alice',
-        'recipient_screen_name': 'Bob',
-        'text': 'Direct Message at %s' % now,
-    }
-    defaults.update(kwargs)
-
-    return DirectMessage(**defaults)
+from .tests import *

tests/test_api.py
@@ -4,7 +4,7 @@ from sys import path
 path.append('../')
 import unittest
 
-from . import create_status, create_direct_message
+from tests import create_status, create_direct_message
 
 from turses.models import Timeline
 from turses.api.base import AsyncApi

tests/test_core.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from sys import path
-path.append('../')
+import sys
+sys.path.append('../')
 import unittest
 
 from mock import Mock
 
-from . import create_status
+from tests import create_status
 from turses.models import TimelineList
 from turses.api.helpers import (
     is_home_timeline,
@@ -35,7 +35,7 @@ class InputHandlerTest(unittest.TestCase):
 
             self.key_handler.handle(key)
 
-            self.failUnless(handler.called)
+            self.assertTrue(handler.called)
 
     def does_not_execute(self, commands):
         """Assert that calling the key handlers `handle` method with all
@@ -47,7 +47,7 @@ class InputHandlerTest(unittest.TestCase):
 
             self.key_handler.handle(key)
 
-            self.failIf(handler.called)
+            self.assertFalse(handler.called)
 
     def key(self, command):
         key, _ = configuration.key_bindings[command]

tests/test_models.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append('../')
 
-from sys import path
-path.append('../')
 import unittest
 from datetime import datetime
 
 from mock import MagicMock
 
 from tests import create_status, create_direct_message
-from tests.test_meta import ActiveListTest
+from test_meta import ActiveListTest
 
 from turses.utils import prepend_at
 from turses.models import is_DM, Timeline, TimelineList
+
 
 
 class StatusTest(unittest.TestCase):
     def test_is_DM(self):
         # status is NOT a DM
         status = create_status()
-        self.failIf(is_DM(status))
+        self.assertFalse(is_DM(status))
 
         dm = create_direct_message()
-        self.failUnless(is_DM(dm))
+        self.assertTrue(is_DM(dm))
 
     # properties
 
@@ -92,7 +93,7 @@ class StatusTest(unittest.TestCase):
         # must return `None`
         status = create_status(user=user)
         recipient_own_tweet = status.dm_recipients_username(user)
-        self.failIf(recipient_own_tweet)
+        self.assertFalse(recipient_own_tweet)
 
     def test_dm_recipients_username_dm(self):
         # authenticating user
@@ -225,7 +226,7 @@ class TimelineTest(ActiveListTest):
         self.assertEqual(self.timeline.unread_count, 0)
 
         # new statuses
-        statuses = [create_status(id=id_num) for id_num in xrange(2, 10)]
+        statuses = [create_status(id=id_num) for id_num in range(2, 10)]
         self.timeline.add_statuses(statuses)
         self.assertEqual(self.timeline.unread_count, len(statuses))
 
@@ -423,11 +424,11 @@ class TimelineListTest(ActiveListTest):
         self.timeline_list = TimelineList()
 
     def test_has_timelines_false_if_empty(self):
-        self.failIf(self.timeline_list.has_timelines())
+        self.assertFalse(self.timeline_list.has_timelines())
 
     def test_has_timelines_true_otherwise(self):
         self.append_timeline()
-        self.failUnless(self.timeline_list.has_timelines())
+        self.assertTrue(self.timeline_list.has_timelines())
 
     def test_null_index_with_no_timelines(self):
         self.assert_null_index()

tests/test_utils.py
@@ -1,3 +1,4 @@
+#
 # -*- coding: utf-8 -*-
 
 from sys import path
@@ -12,21 +13,21 @@ class UtilsTest(unittest.TestCase):
     def test_is_username(self):
         valid = ['dialelo', 'mental_floss', '4n_4Wfu1_US3RN4M3']
         for user in valid:
-            self.failUnless(is_username(user))
+            self.assertTrue(is_username(user))
 
         invalid = ['-asd', 'adsd?']
 
         for user in invalid:
-            self.failIf(is_username(user))
+            self.assertFalse(is_username(user))
 
     def test_is_hashtag(self):
         valid = ['#turses', '#cúrcuma', '#4n_4Wfu1_H45hT46']
         for hashtag in valid:
-            self.failUnless(is_hashtag(hashtag))
+            self.assertTrue(is_hashtag(hashtag))
 
         invalid = ['s#turses', '#']
         for hashtag in invalid:
-            self.failIf(is_hashtag(hashtag))
+            self.assertFalse(is_hashtag(hashtag))
 
     def test_sanitize_username(self):
         dirty_and_clean = [

tests/tests.py
@@ -1,0 +1,34 @@
+from datetime import datetime
+from time import sleep
+
+from turses.models import Status, DirectMessage
+
+
+def now():
+    sleep(0.001)
+    return datetime.now()
+
+
+def create_status(**kwargs):
+    defaults = {
+        'id': 1,
+        'created_at': now(),
+        'user': 'testbot',
+        'text': 'Status created at %s' % now,
+    }
+    defaults.update(**kwargs)
+
+    return Status(**defaults)
+
+
+def create_direct_message(**kwargs):
+    defaults = {
+        'id': 1,
+        'created_at': now(),
+        'sender_screen_name': 'Alice',
+        'recipient_screen_name': 'Bob',
+        'text': 'Direct Message at %s' % now,
+    }
+    defaults.update(kwargs)
+
+    return DirectMessage(**defaults)

commit be0961b51f502d49fd9e2e5253ac130e543a31c7

turses: config: fix py3k conversion
The py3k conversion didn't properly fix the config loading code.

Fixes: bfc1d3160ef7 ("python3 port")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

turses/config.py
@@ -756,8 +756,7 @@ class Configuration(object):
                 elif style == 'statuses_in_user_info':
                     self.styles[style] = conf.getint(SECTION_STYLES, style)
                 else:
-                    self.styles[style] = str(
-                        conf.get(SECTION_STYLES, style), 'utf-8')
+                    self.styles[style] = conf.get(SECTION_STYLES, style)
 
     def _parse_debug(self, conf):
         if conf.has_option(SECTION_DEBUG, 'logging_level'):